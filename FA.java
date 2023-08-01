2d25d70feca884b4f0416eada730e9eb6b384fe8
Nikhil Kowdle <72424123+StormWolf49@users.noreply.github.com>
2023-07-31 20:05:41 -0700

    Add files via upload

---
+++ FA.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FA {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner data = new Scanner(new File("data.txt"));
+
+
+    }
+    
+    
+}
